3dc15cca6b8b749b46187d4d671b03b19fcf0015
adwd <ecdt.mn@gmail.com>
2020-02-15 18:16:26 +0900

    add error handling

--- parser/parser.go
+++ parser/parser.go
@@ -1,26 +1,41 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/adwd/monkey/ast"
 	"github.com/adwd/monkey/lexer"
 	"github.com/adwd/monkey/token"
 )
 
 type Parser struct {
-	l *lexer.Lexer
+	l      *lexer.Lexer
+	errors []string
 
 	curToken  token.Token
 	peekToken token.Token
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
 
 	// 2つトークンを読み込む。curTokenとpeekTokenの両方がセットされる
 	p.nextToken()
 	p.nextToken()
 
 	return p
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
 
 func (p *Parser) nextToken() {
@@ -87,5 +102,6 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		return true
 	}
 
+	p.peekError(t)
 	return false
 }
